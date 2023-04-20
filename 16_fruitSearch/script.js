6e6e9d3d6b51b12a87464a05c9192c9d4dd06d8f
Sergio Robledo <sergiorobledo2014@gmail.com>
2023-04-20 00:32:14 -0500

    Completed Fruit Search Project

---
+++ 16_fruitSearch/script.js
@@ -0,0 +1,36 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+
+	// TODO
+	const results = fruit.filter(f => f.toLowerCase().includes(str.toLowerCase()));
+	showSuggestions(results, str)
+};
+
+function searchHandler(e) {
+	// TODO
+	search(input.value);
+};
+
+function showSuggestions(results, inputVal) {
+
+	// TODO
+	// need to clear previous li & add current suggestions
+	suggestions.innerHTML = '';
+	if (inputVal) {results.forEach(suggestion => suggestions.innerHTML += '<li>'+suggestion+'</li')};
+	
+
+};
+
+function useSuggestion(e) {
+	// TODO
+	suggestions.innerHTML = '';
+	input.value = e.target.innerText;
+
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
